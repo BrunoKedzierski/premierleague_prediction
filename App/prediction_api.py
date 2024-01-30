27c56e8e4468155d1bdd8c4df4cd2b11175f66f7
BrunoKedzierski <bruno.kedzierski@wp.pl>
2024-01-30 18:51:42 +0100

    full app

--- App/prediction_api.py
+++ App/prediction_api.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 import pickle
 from football_game import Football_Game
 import pandas as pd
-import os
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI
 import uvicorn
 
+
+
+## Klasa pomocnicza - wczytywanie modeli
 class ModelHandler:
     def __init__(self):
         self.loaded_model = None
@@ -13,25 +15,31 @@ class ModelHandler:
         self.model_artifact =  None
 
     def load_models(self):
-        self.loaded_model = pickle.load(open('..\\model\\xgboost_model.pkl', 'rb'))
-        self.loaded_encoder =pickle.load(open('..\\model\\encoder.pkl', 'rb'))
+        self.loaded_model = pickle.load(open('model\\xgboost_model.pkl', 'rb'))
+        self.loaded_encoder =pickle.load(open('model\\encoder.pkl', 'rb'))
 
 app = FastAPI()
 model_handler = ModelHandler()
 
+
+#Zaladuj modele na start
 @app.on_event("startup")
 def load_models():
     model_handler.load_models()
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+#Informacje o modelu
 @app.get("/modelinfo")
 def show_model_info():
     return {"model_type": type(model_handler.loaded_model).__name__}
 
-@app.post("/predict/client")
+
+#Predykcja
+@app.post("/predict/match")
 def predict_client(data: Football_Game):
     
     data = data.dict()
@@ -41,19 +49,17 @@ def predict_client(data: Football_Game):
     print(dframe)
     encoded_columns = model_handler.loaded_encoder.transform(dframe[['HomeTeam', 'AwayTeam', 'HTR']])
 
-    # Get feature names for the transformed data
+    
     feature_names = model_handler.loaded_encoder.get_feature_names_out(input_features=['HomeTeam', 'AwayTeam', 'HTR'])
 
-    # Create DataFrame for the encoded columns
+    
     data_encoded = pd.DataFrame(encoded_columns, columns=feature_names)
 
-    # Add missing columns with 0 values to match the structure of the training data
+  
     missing_columns = set(model_handler.loaded_encoder.get_feature_names_out()) - set(data_encoded.columns)
     for col in missing_columns:
         data_encoded[col] = 0.0
 
-    print(data_encoded.info())
-    # Concatenate the encoded columns with the remaining columns from the original DataFrame
     data_encoded = pd.concat([dframe.drop(columns=['HomeTeam', 'AwayTeam', 'HTR']), data_encoded], axis=1)
 
     print("After concat")
@@ -70,7 +76,7 @@ def predict_client(data: Football_Game):
     'prediction': prediction.tolist()[0],
     'probabilityHome': prob.flatten().tolist()[0],
     'probabilityDraw': prob.flatten().tolist()[1],
-    'probabilityWin': prob.flatten().tolist()[2],
+    'probabilityAway': prob.flatten().tolist()[2],
 }
 
 if __name__ == '__main__':
